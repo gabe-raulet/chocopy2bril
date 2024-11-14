45b0d76e2fe323f87ab5399711aff425841c6550
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-11-14 13:57:37 -0800

    split up tests

--- tester.py
+++ tester.py
@@ -3,23 +3,34 @@ from interp import Interpreter
 
 class TestInterpreter(unittest.TestCase):
 
-    def test_binary_ops(self):
+    def test_simple(self):
         for x in range(15):
-            for y in range(1, 15):
+            for y in range(15):
                 self.assertEqual(x + y, Interpreter.interpret(f"{x} + {y}"))
                 self.assertEqual(x * y, Interpreter.interpret(f"{x} * {y}"))
-                for z in range(1, 15):
+
+    def test_compound(self):
+        for x in range(15):
+            for y in range(15):
+                for z in range(15):
                     self.assertEqual(x + y + z, Interpreter.interpret(f"{x} + {y} + {z}"))
                     self.assertEqual(x * y * z, Interpreter.interpret(f"{x} * {y} * {z}"))
+
+    def test_precedence(self):
+        for x in range(15):
+            for y in range(15):
+                for z in range(15):
                     self.assertEqual(x + y * z, Interpreter.interpret(f"{x} + {y} * {z}"))
                     self.assertEqual(x * y + z, Interpreter.interpret(f"{x} * {y} + {z}"))
+
+    def test_association(self):
+        for x in range(15):
+            for y in range(15):
+                for z in range(15):
                     self.assertEqual((x + y) * z, Interpreter.interpret(f"({x} + {y}) * {z}"))
+                    self.assertEqual(x * (y + z), Interpreter.interpret(f"{x} * ({y} + {z})"))
+                    self.assertEqual((x * y) + z, Interpreter.interpret(f"({x} * {y}) + {z}"))
                     self.assertEqual(x + (y * z), Interpreter.interpret(f"{x} + ({y} * {z})"))
-                    for w in range(1, 15):
-                        self.assertEqual((x + y) * (w + z), Interpreter.interpret(f"({x} + {y}) * ({w} + {z})"))
-                        self.assertEqual((x + y) + (w * z), Interpreter.interpret(f"({x} + {y}) + ({w} * {z})"))
-                        self.assertEqual((x * y) + (w + z), Interpreter.interpret(f"({x} * {y}) + ({w} + {z})"))
-                        self.assertEqual(((x + y + w) * z), Interpreter.interpret(f"({x} + {y} + {w}) * {z}"))
 
 if __name__ == "__main__":
     unittest.main()
