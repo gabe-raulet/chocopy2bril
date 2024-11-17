a2400f9d9be03226959641deb2a45334320158d3
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-11-17 15:59:48 -0800

    more interesting example

--- ex2.py
+++ ex2.py
@@ -1,3 +1,10 @@
-var1 = 12
-var2 = 23
-var3 = var1 + var2
+a = 10
+b = 25
+c = 35
+d = 10 * (a + b + c)
+c = 30 - d
+e = 1 - c
+a = 3*e  + 51
+b = a * b + c - d + (d * (3 - a - b + 100))
+print(a)
+print(b)
