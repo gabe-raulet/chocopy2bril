8598f6804df34a71c40be9732cd33727463a4ee8
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-11-25 17:22:20 -0800

    work on integers only for now, do control flow, functions etc.

--- small2/parse.py
+++ small2/parse.py
@@ -31,24 +31,21 @@ class AstProgram(Ast):
 
     def get_code(self):
         instrs = []
-        for var_def in self.var_defs:
-            instrs.append(var_def.get_instr())
-        for stmt in self.stmts:
-            instrs += stmt.get_code()
+        for var_def in self.var_defs: instrs.append(var_def.get_instr())
+        for stmt in self.stmts: instrs += stmt.get_code()
         return instrs
 
 class AstVarDef(Ast):
 
-    def __init__(self, var_id, var_type, literal):
-        self.id = var_id
-        self.type = var_type
-        self.literal = literal
+    def __init__(self, name, lit):
+        self.id = name
+        self.lit = lit
 
     def __repr__(self):
-        return f"AstVarDef(id={self.id}, type={self.type}, literal={self.literal})"
+        return f"AstVarDef(id={self.id}, lit={self.lit})"
 
     def get_instr(self):
-        return {"dest" : self.id, "type" : self.type, "value" : self.literal, "op" : "const"}
+        return {"dest" : self.id, "type" : "int", "value" : self.literal, "op" : "const"}
 
 class AstPrint(Ast):
 
@@ -61,36 +58,33 @@ class AstPrint(Ast):
     def get_code(self):
         global reg
         dest = reg.next()
-        inst1 = {"dest" : dest, "type" : self.expr.type}
+        def_inst = {"dest" : dest, "type" : "int"}
         if isinstance(self.expr, AstLiteral):
-            inst1["op"] = "const"
-            inst1["value"] = self.expr.lit
+            def_inst["op"] = "const"
+            def_inst["value"] = self.expr.lit
         elif isinstance(self.expr, AstVariable):
-            inst1["op"] = "id"
-            inst1["args"] = [self.expr.id]
+            def_inst["op"] = "id"
+            def_inst["args"] = [self.expr.id]
         else:
             raise Exception("Invalid expression for print")
-        inst2 = {"op" : "print", "args" : [dest]}
-        return [inst1, inst2]
+        print_inst = {"op" : "print", "args" : [dest]}
+        return [def_inst, print_inst]
 
 class AstLiteral(Ast):
 
-    def __init__(self, ctype, literal):
-        self.type = ctype
-        self.lit = literal
+    def __init__(self, lit):
+        self.lit = lit
 
     def __repr__(self):
-        return f"AstLiteral(type={self.type}, literal={self.lit})"
+        return f"AstLiteral({self.lit})"
 
 class AstVariable(Ast):
 
-    def __init__(self, var_type, var_id):
-        self.type = var_type
-        self.id = var_id
+    def __init__(self, name):
+        self.id = name
 
     def __repr__(self):
-        return f"AstVariable(type={self.type}, id={self.id})"
-
+        return f"AstVariable({self.id})"
 
 class Parser(object):
 
@@ -118,6 +112,9 @@ class Parser(object):
         else: self.error()
         return matchee
 
+    def match_newline(self):
+        self.match("NEWLINE")
+
     def get_program(self):
         var_defs = self.get_var_defs()
         stmts = self.get_stmts()
@@ -133,15 +130,11 @@ class Parser(object):
         id_token = self.match(Token.ID)
         self.match(Token.COLON)
         type_token = self.match(Token.TYPE)
+        assert type_token.value == "int" # TODO temporary
         self.match(Token.ASSIGN)
-        if type_token.value == "int":
-            lit_token = self.match(Token.NUM)
-        elif type_token.value == "bool":
-            lit_token = self.match(Token.BOOL)
-        else:
-            self.error() # TODO: str token if we want to do that eventually
-        self.match("NEWLINE")
-        return AstVarDef(id_token.value, type_token.value, lit_token.value)
+        lit_token = self.match(Token.NUM)
+        self.match_newline()
+        return AstVarDef(id_token.value, lit_token.value)
 
     def get_stmts(self):
         stmts = []
@@ -154,31 +147,38 @@ class Parser(object):
             if self.token().lexeme() == "print":
                 self.match(Token.KEYWORD)
                 self.match(Token.LPAREN)
-                stmt = AstPrint(expr=self.get_expr())
+                stmt = AstPrint(self.get_expr())
                 self.match(Token.RPAREN)
             else:
-                self.error("Only keyword implemented is print") # TODO
+                self.error("Only keyword implemented is print, currently") # TODO
         else:
-            self.error("Only stmt implemented is AstPrint") # TODO
-        self.match("NEWLINE")
+            self.error("Only stmt implemented is AstPrint currently") # TODO
+        self.match_newline()
         return stmt
 
     def get_expr(self):
         expr = None
         if self.token() == Token.NUM:
             result = self.match(Token.NUM)
-            expr = AstLiteral("int", result.value)
+            expr = AstLiteral(result.value)
         elif self.token() == Token.ID:
             result = self.match(Token.ID)
-            expr = AstVariable("int", result.value) # TODO: need to be look up type in symbol table
+            expr = AstVariable(result.value)
         else:
-            self.error("only single token expressions implemented so far")
+            self.error()
         return expr
 
-if __name__ == "__main__":
-    tokens = list(lex_text(sys.stdin.read()))
-    parser = Parser(tokens)
-    program = parser.get_program()
-    func = {"name": "main", "instrs" : program.get_code()}
-    prog = {"functions" : [func]}
-    json.dump(prog, sys.stdout, indent=4)
+def read_text(fname):
+    return open(fname).read()
+
+#  if __name__ == "__main__":
+    #  tokens = list(lex_text(sys.stdin.read()))
+    #  parser = Parser(tokens)
+    #  program = parser.get_program()
+    #  func = {"name": "main", "instrs" : program.get_code()}
+    #  prog = {"functions" : [func]}
+    #  json.dump(prog, sys.stdout, indent=4)
+
+tokens = list(lex_text(read_text("t4.py")))
+parser = Parser(tokens)
+
---
+++ small2/t4.py
@@ -0,0 +1,14 @@
+a: int = 100
+b: int = 50
+c: int = 0
+
+print(a)
+print(b)
+print(c)
+
+c = 2 * b
+print(a)
+print(b)
+print(c)
+
+#  d = (a == b)
