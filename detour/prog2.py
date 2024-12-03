15dbd02a171474627b0f2ddf094f228886eaeacb
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-12-02 16:44:31 -0800

    excellent

---
+++ detour/prog2.py
@@ -0,0 +1,16 @@
+# prog2.py
+#
+# var_defs: yes
+# func_defs: no
+# stmts: no
+
+var1: int = 15
+var2: bool = False
+
+var3: bool = None
+
+var4: int = 66
+var5: bool = True
+
+var6: int = None
+
---
+++ detour/prog3.py
@@ -0,0 +1,13 @@
+# prog3.py
+#
+# var_defs: yes
+# func_defs: no
+# stmts: yes
+
+var1: int = 14
+var2: bool = False
+
+print(var1)
+print(var2)
+print(13)
+print(True)
