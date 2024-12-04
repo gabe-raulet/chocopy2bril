453e05576a68761dac493f316d59a00b2d6a2b3f
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-12-04 13:33:27 -0800

    prog

--- structured/prog.py
+++ structured/prog.py
@@ -6,5 +6,16 @@ def func1(a: int, b: bool) -> int:
     var1: bool = False
     return a
 
+v: int = 9154235
+
+def func2(a: int):
+    print(a)
+
 var2 = func1(15, var3)
 print(var2)
+
+func2(14)
+func2(var2)
+
+print(v)
+func2(v)
