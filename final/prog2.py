98bfca02f9fb4a881271484b1e9bc5631b1c28c9
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-12-06 15:02:47 -0800

    good

---
+++ final/prog2.py
@@ -0,0 +1,18 @@
+var1: int = 992
+var2: int = None
+var3: bool = True
+var4: bool = None
+
+def f1():
+    return
+
+def f2() -> int:
+    return 2
+
+def f3(a: int) -> int:
+    return a
+
+def f4(a: int, b: bool) -> bool:
+    var1: int = 151
+    var2: bool = None
+    return var2
